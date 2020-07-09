1ab52dc75fc9b5088ddb0722ea63f53f17a98e6e
Tiauna Paul <tiaunaj.tsg@gmail.com>
2020-07-09 22:15:15 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,7 +2,7 @@ def speak_to_grandma(phrase)
   
   if phrase.upcase != phrase
     "HUH?! SPEAK UP SONNY!"
-  else if phrase == "I LOVE YOU GRANDMA!"
+  elseif phrase == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
   else
     "NO, NOT SINCE 1938!"
